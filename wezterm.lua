a6df67852497f7f617971f683dd860de9ecaf74f
Jonas Gao <gaopinsong@live.cn>
2024-07-30 17:15:29 +0800

    Use mica style.

--- wezterm.lua
+++ wezterm.lua
@@ -6,4 +6,8 @@ end
 config.font_size = 10
 config.window_decorations = "RESIZE"
 config.default_prog = { 'pwsh.exe' }
+-- Config window opacity and backdrop
+config.window_background_opacity = 0
+config.win32_system_backdrop = "Mica"
+-- Finish
 return config
